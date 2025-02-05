0936c422ff49f2b0162b1e6769a2bfc24b3adae4
Talmacel Larisa <larisa-maria.talmacel@student.tuiasi.ro>
2025-02-05 09:57:38 +0200

    Added get_recipe_ summary endpoint

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,7 +1,7 @@
 const API_KEY = process.env.API_KEY
 
-export const searchRecipes = async (searchTerm:string, page:number) => {
-    if (!API_KEY){
+export const searchRecipes = async (searchTerm: string, page: number) => {
+    if (!API_KEY) {
         throw new Error("API key not found");
     }
 
@@ -12,18 +12,38 @@ export const searchRecipes = async (searchTerm:string, page:number) => {
         apiKey: API_KEY,
         query: searchTerm,
         number: "5",
-        offset: ((page-1) * 5).toString(),
+        offset: ((page - 1) * 5).toString(),
     };
 
     url.search = new URLSearchParams(queryParams).toString()
 
-    try{
+    try {
         const searchResponse = await fetch(url.toString())
         const result = await searchResponse.json()
 
         return result
-    }catch(error)
-    {
+    } catch (error) {
         console.error(error)
     }
 }
+
+export const getRecipeSummary = async (recipeId: string) => {
+    if (!API_KEY) {
+        throw new Error("API key not found");
+    }
+
+    const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`);
+    const params = {
+        apiKey: API_KEY
+    }
+
+    url.search = new URLSearchParams(params).toString();
+
+    try {
+        const recipeSummary = await fetch(url);
+        const jsonSummary = await recipeSummary.json();
+        return jsonSummary;
+    } catch (error) {
+        console.error(error);
+    }
+};
